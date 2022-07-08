d588a892ef39997973182a789c51b49c388e3fbf
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-08 16:16:22 -0300

    migrations

--- db/migrate/20220708181230_create_coins.rb
+++ db/migrate/20220708181230_create_coins.rb
@@ -2,7 +2,7 @@ class CreateCoins < ActiveRecord::Migration[5.2]
   def change
     create_table :coins do |t|
       t.string :description
-      t.strgin :acronym
+      t.string :acronym
       t.string :url_image
 
       t.timestamps
