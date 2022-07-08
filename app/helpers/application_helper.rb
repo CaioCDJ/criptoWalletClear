ff96a260657c3e8dd1ca09892ab5a238553f5d3f
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-08 17:45:51 -0300

    Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # helpers criados 
+  #
+  # funcoes que podem ser chamadas em 
+  # multiplas partes do projeto
+  #
+  # helper no aplication = " funcao global"
+
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def nome_aplicacao
+    "Criptowallet"
+  end
 end
