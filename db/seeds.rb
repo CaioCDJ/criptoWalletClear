299faa7959d3213a0d1426a00453dfb2e8b9d953
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-11 17:42:07 -0300

    refactoring seeds and Rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas" )
+      spinner.auto_spin   
+# facilidade
+coins = [
+  {
+   description: "Bitcoin",
+    acronym:"BTC",
+    url_image:"https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Emblem.png"
+  },
+  {
+    description: "Ethereum",
+    acronym:"ETH",
+    url_image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
+  },
+  {
+    description: "Monero",
+    acronym:"XMR",
+    url_image:"https://seeklogo.com/images/M/monero-logo-4EB9795194-seeklogo.com.png"
+  }
+]
 
-Coin.create!(
- description: "Bitcoin",
- acronym:"BTC",
- url_image:"https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Emblem.png"
-)
-Coin.create!(
- description: "Ethereum",
- acronym:"ETH",
- url_image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
-)
-Coin.create!(
- description: "Monero",
- acronym:"XMR",
- url_image:"https://seeklogo.com/images/M/monero-logo-4EB9795194-seeklogo.com.png"
-)
 
+coins.each do |coin|
+  sleep(1)
+  Coin.find_or_create_by!(coin)
+end
+
+
+spinner.success('(Concluido com sucesso!)');
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,7 +1,26 @@
 namespace :dev do
   desc "Configura o ambiente de desenvolvimento"
   task setup: :environment do
-   puts %x(rails db:drop db:create db:migrate db:seed)
+  # funcoes bem mais inchutas 
+    if Rails.env.development?
+      show_spinner("Apagando DB..."){ %x(rails db:drop )}
+      show_spinner("Criando DB..."){ %x(rails db:create ) }
+      show_spinner("Migrando DB..."){ %x(rails db:migrate ) }
+      show_spinner("Semeando DB..."){ %x(rails db:seed ) }
+    else
+      puts "Você não está em ambiente de desenvolvimento!"
+    end
   end
 
+  #   yield um bloco de codigo que sera passado 
+  # quando a funcao chamada
+  #
+  # private above the func
+  private 
+  def show_spinner(msg_start)
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}" )
+      spinner.auto_spin    
+      yield
+      spinner.success('(Concluido com sucesso!)');
+  end
 end
