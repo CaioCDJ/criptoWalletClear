0d816c0c14f4ddc7452a13ec772f68a63da46977
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-11 14:32:24 -0300

    Partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,14 @@ module ApplicationHelper
     "Criptowallet"
   end
 
-  # Constantes tambem funcionam
-  NOME = 'Criptowallet'
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+
+    elsif Rails.env.production?
+      "Produção"
+    else 
+      "Teste"
+    end
+  end
 end
