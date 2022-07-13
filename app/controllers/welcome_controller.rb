92d6cdb81d0e6fe4d1db9c6bab54d6cb3255ada6
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-13 14:04:00 -0300

    Commit and langChange

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    # Setting cookies and session
+    cookies[:curso] = "Curso de Ruby on Rais - Jackson Pires [Cookie]"
+    session[:curso] = "Curso de Ruby on Rais - Jackson Pires [Session]"
     @nome = params[:nome]
     @curso = params[:curso]
   end
